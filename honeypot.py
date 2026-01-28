cace6f492266cc08f7f9ce7cba03eedfdd279366
Punsith Vidumal <punsithvidumal@gmail.com>
2026-01-28 21:31:57 +0530

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -1,2 +1,18 @@
 import socket
 import logging
+
+logging.basicConfig(filename='honeypot.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+def start_honeypot(ip='0.0.0.0', port=2222):
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+
+    try:
+        s.bind((ip, port))
+        s.listen(5)
+        print(f"[*] Honeypot listening on {ip}:{port}...")
+    except Exception as e:
+        print(f"Error: {e}")
+        return
+
