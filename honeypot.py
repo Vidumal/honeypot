cf942da9f63d9cca9e743c6a3e1e166856001722
Punsith Vidumal <punsithvidumal@gmail.com>
2026-01-30 23:39:34 +0530

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -1,13 +1,14 @@
 import socket
 import logging
 
+
 logging.basicConfig(filename='honeypot.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 def start_honeypot(ip='0.0.0.0', port=2222):
-    
+   
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-
+    
     try:
         s.bind((ip, port))
         s.listen(5)
@@ -16,25 +17,26 @@ def start_honeypot(ip='0.0.0.0', port=2222):
         print(f"Error: {e}")
         return
 
-while True:
+    while True:
        
         client_socket, addr = s.accept()
         print(f"[!] Alert: Connection from {addr[0]}:{addr[1]}")
         logging.info(f"Connection from: {addr[0]}")
 
         try:
-        
+           
             client_socket.send(b"Welcome to Ubuntu 20.04 LTS. Login: ")
             
-            
+           
             data = client_socket.recv(1024)
             print(f"[+] Attacker tried: {data.decode('utf-8').strip()}")
             logging.info(f"Payload: {data.decode('utf-8').strip()}")
             
-        
+            
             client_socket.close()
         except Exception as e:
             client_socket.close()
+
 if __name__ == "__main__":
    
     start_honeypot()
